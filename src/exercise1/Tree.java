b166bc0c4a35713404776d5ad1a11884fae19d7d
YankoB <yankoo_bobev@abv.bg>
2020-09-30 14:00:22 +0000

    Uebung1

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -3,10 +3,10 @@ package exercise1;
 public class Tree {
 
   public static void main(String[] args) {
-    System.out.print("    *    \n");
-    System.out.print("   ***   \n");
-    System.out.print("  *****  \n");
-    System.out.print(" ******* \n");
+    System.out.print("    *\n");
+    System.out.print("   ***\n");
+    System.out.print("  *****\n");
+    System.out.print(" *******\n");
     System.out.print("*********\n");
     System.out.print("   ***   \n");
   }
